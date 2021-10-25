dfdeab011c23d95e35ec7ebbc8753b6c6f7d46d9
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2021-10-25 16:21:47 +0300

    firebase config added.

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCrOHEIZac7Q_6Q1LfmW_7c7vRxSE-GgmA",
+  authDomain: "discord-clone-6639b.firebaseapp.com",
+  projectId: "discord-clone-6639b",
+  storageBucket: "discord-clone-6639b.appspot.com",
+  messagingSenderId: "844318280327",
+  appId: "1:844318280327:web:279ec73396add56c620622",
+  measurementId: "G-8ZX4VYB51J"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore(); 
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider }
+export default db;
