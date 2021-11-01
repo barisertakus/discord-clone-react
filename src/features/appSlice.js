2d75a139f9aba8ca3ee1d733e182b48ee3ecc7fa
barisertakus <baris.ertakus@ogr.sakarya.edu.tr>
2021-11-02 02:06:10 +0300

    set channel info added to appslice.

--- src/features/appSlice.js
+++ src/features/appSlice.js
@@ -9,8 +9,9 @@ export const appSlice = createSlice({
   name: 'app',
   initialState,
   reducers: {
-    setChannelId: (state,action) => {
-      state.app = action.payload
+    setChannelInfo: (state,action) => {
+      state.channelId = action.payload.channelId;
+      state.channelName = action.payload.channelName;
     },
     logout: (state) => {
       state.app = null;
@@ -18,7 +19,7 @@ export const appSlice = createSlice({
   },
 });
 
-export const { setChannelId, logout } = appSlice.actions;
+export const { setChannelInfo, logout } = appSlice.actions;
 
 export const selectChannelId = (state) => state.app.channelId;
 export const selectChannelName = (state) => state.app.channelName;
